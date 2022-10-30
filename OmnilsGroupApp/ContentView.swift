000d8fcc798ee5825c4973cbd4f9d5a879266cdb
zannatul-fardous <zannat12.fardous@gmail.com>
2022-10-30 19:00:35 +0100

    initial commit

---
+++ OmnilsGroupApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OmnilsGroupApp
+//
+//  Created by Zannatul Pinky on 2022-10-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OmnilsGroupApp/OmnilsGroupAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmnilsGroupAppApp.swift
+//  OmnilsGroupApp
+//
+//  Created by Zannatul Pinky on 2022-10-28.
+//
+
+import SwiftUI
+
+@main
+struct OmnilsGroupAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
